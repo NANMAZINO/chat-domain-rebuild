5d3f7fbf05c5058187270a73b2402ce28579ccad
Xtraa777 <mazino0588@gmail.com>
2026-04-02 01:45:09 +0900

    Initial local project

---
+++ src/main/java/io/github/nanmazino/chatrebuild/ChatRebuildApplication.java
@@ -0,0 +1,13 @@
+package io.github.nanmazino.chatrebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRebuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRebuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/nanmazino/chatrebuild/ChatRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nanmazino.chatrebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRebuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
