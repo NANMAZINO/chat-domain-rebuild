19cddbea27a2b4fe1093a0c148e9ec91d53ad925
Xtraa777 <mazino0588@gmail.com>
2026-04-03 04:01:37 +0900

    infra: local/test 환경 분리 및 MySQL·Redis 연결 설정

--- src/test/java/io/github/nanmazino/chatrebuild/ChatRebuildApplicationTests.java
+++ src/test/java/io/github/nanmazino/chatrebuild/ChatRebuildApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.nanmazino.chatrebuild;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChatRebuildApplicationTests {
 
     @Test
